6ef28828f42cbfc8f478532e55ff63f555edfa89
Owyn Richen <orichen@Administrators-MacBook-Pro.local>
2011-03-23 08:21:42 -0700

    Initial Commit

---
+++ FootGame/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  FootGame
+//
+//  Created by Owyn Richen on 3/23/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
