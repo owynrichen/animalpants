e8ed86e5b082a2c9d5d5a32747a7ad2ad68cec3f
Owyn Richen <orichen@whitepages.com>
2013-01-30 21:56:56 -0800

    I don't know what the hell caused all of these extra directories, but I'm removing them...

--- FootGame/FootGame/FootGame/FootGame/FootGame/FootGame/FootGame/FootGame/FootGame/FootGame/FootGame/FootGame/FootGame/FootGame/FootGame/FootGame/FootGame/FootGame/FootGame/convert_and_resize.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-echo cp "/Users/orichen/Dropbox/Animals\ App\ illustrations/$1" "bg-ipadhd.jpg"
-cp /Users/orichen/Dropbox/Animals\ App\ illustrations/$1 bg-ipadhd.jpg
-png=bg_$2-ipadhd.png
-
-convert -format PNG -quality 5 bg-ipadhd.jpg $png
-../../../../resize.sh $png
-rm bg-ipadhd.jpg
